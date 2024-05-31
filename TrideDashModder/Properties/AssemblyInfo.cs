74696e2e876d2f7bfcab63f50e7ac252562b1267
endert1099 <97123144+endert1099@users.noreply.github.com>
2024-05-31 19:39:45 +1000

    Changed to v0.2.2 assembly

--- TrideDashModder/Properties/AssemblyInfo.cs
+++ TrideDashModder/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices.ComTypes;
 using MelonLoader;
 using TrideDashModder; // The namespace of your mod class
 // ...
-[assembly: MelonInfo(typeof(Mods), "TrideHack", "0.2", "EnderGames")]
+[assembly: MelonInfo(typeof(Mods), "TrideHack", "0.2.2", "EnderGames")]
 [assembly: MelonGame("DefaultCompany", "Tride Dash")]
 
 // General Information about an assembly is controlled through the following
@@ -38,5 +38,5 @@ using TrideDashModder; // The namespace of your mod class
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2")]
-[assembly: AssemblyFileVersion("0.2")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.2")]
